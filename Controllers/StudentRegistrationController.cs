9aefe94572568984d2d1b5a14d77364e188ac9da
Mudibo <sean.mudibo@strathmore.edu>
2026-01-23 14:29:16 +0300

    Added logging to student registration service and refactored code (try/catch)

--- Controllers/StudentRegistrationController.cs
+++ Controllers/StudentRegistrationController.cs
@@ -5,21 +5,29 @@ using StudentPayments_API.Services.Interfaces;
 
 namespace StudentPayments_API.Controllers;
 
+//Controller to receive request, delegate logic to service, and return valid HTTP response.
+
 [ApiController]
 [Route("api/[controller]")]
 public class StudentRegistrationController : ControllerBase
 {
     private readonly IStudentRegistrationService _registrationService;
+    
+    //Constructor receives an implementation of IStudentRegistrationService via Dependency Injection
     public StudentRegistrationController(IStudentRegistrationService registrationService)
     {
         _registrationService = registrationService;
     }
+
+    //Expose POST endpoint: POST api/StudentRegistration/register
     [HttpPost("register")]
     public async Task<IActionResult> RegisterStudent([FromBody] StudentRegistrationDto dto)
     {
+        //Call RegisterStudentAsync to attempt to register the student
         var (success, message, student) = await _registrationService.RegisterStudentAsync(dto);
         if (!success)
         {
+            //If registration fails, return 400 bad request
             return BadRequest(new
             {
                 message
@@ -27,6 +35,7 @@ public class StudentRegistrationController : ControllerBase
         }
         return Ok(new
         {
+            //If registration succeeds, return 200 ok with student details
             message,
             student
         });
