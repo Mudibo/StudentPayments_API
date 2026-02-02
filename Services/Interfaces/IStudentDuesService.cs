2e86028407a053f7121999fdfbce7579ebd1a1f7
Mudibo <sean.mudibo@strathmore.edu>
2026-02-02 13:11:12 +0300

    Added functionality to get balance of a student

--- Services/Interfaces/IStudentDuesService.cs
+++ Services/Interfaces/IStudentDuesService.cs
@@ -8,4 +8,5 @@ namespace StudentPayments_API.Services.Interfaces;
 public interface IStudentDuesService
 {
     Task<AddStudentDuesResponseDto<StudentDues>> AddDuesAsync(AddStudentDuesDto dto);
+    Task<decimal> GetStudentBalanceAsync(GetStudentBalanceRequestDto dto);
 }
