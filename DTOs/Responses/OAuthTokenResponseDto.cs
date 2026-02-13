5783641f85117a9aeb5936b6cb12cc09745d91fb
Mudibo <sean.mudibo@strathmore.edu>
2026-02-13 15:58:19 +0300

    Refactored OAuth Controller to use DTO responses

--- DTOs/Responses/OAuthTokenResponseDto.cs
+++ DTOs/Responses/OAuthTokenResponseDto.cs
@@ -3,9 +3,10 @@ using StudentPayments_API.Models.Enums;
 
 public class OAuthTokenResponseDto
 {
-    public bool Success { get; set; }
-    public OAuthErrorEnum Error { get; set; }
-    public string AccessToken { get; set; }
-    public int ExpiresIn { get; set; }
-    public string Scope { get; set; }
+    public string access_token {get;set;}
+    public string token_type {get;set;}
+    public int expires_in {get;set;}
+    public string scope {get;set;}
+    public string error {get;set;}
+    public string error_description {get;set;}
 }
---
+++ Models/Enums/OAuthErrorEnumExtensions.cs
@@ -0,0 +1,19 @@
+
+using StudentPayments_API.Models.Enums;
+
+public static class OAuthErrorEnumExtensions
+{
+    public static string ToOAuthErrorString(this OAuthErrorEnum error)
+    {
+        return error switch
+        {
+            OAuthErrorEnum.InvalidClient => "invalid_client",
+            OAuthErrorEnum.InvalidScope => "invalid_scope",
+            OAuthErrorEnum.TemporarilyUnavailable => "temporarily_unavailable",
+            OAuthErrorEnum.ServerError => "server_error",
+            OAuthErrorEnum.UnsupportedGrantType => "unsupported_grant_type",
+            OAuthErrorEnum.InvalidRequest => "invalid_request",
+            _ => null
+        };
+    }
+}
